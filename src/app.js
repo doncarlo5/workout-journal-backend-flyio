369e20ddc60b517260d67cad11bc63f6b87f35df
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-25 09:08:44 +0200

    Refactor main file structure and update dependencies

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+require("dotenv").config();
+
+require("./database");
+
+const middlewares = require("./middlewares");
+const api = require("./api");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+
+const FRONTEND_URL = process.env.FRONTEND_ORIGIN_URL || "http://localhost:3000";
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
+  });
+});
+
+app.use("/api", api);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,4 @@
-const express = require("express");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-
-require("dotenv").config();
-
-require("./database");
-
-const middlewares = require("./middlewares");
-const api = require("./api");
-
-const app = express();
-
-app.use(morgan("dev"));
-app.use(helmet());
-
-const FRONTEND_URL = process.env.FRONTEND_ORIGIN_URL || "http://localhost:3000";
-
-app.use(cors());
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.get("/", (req, res) => {
-  res.json({
-    message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
-  });
-});
-
-app.use("/api", api);
-
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
+const app = require("./app");
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
@@ -41,5 +6,3 @@ app.listen(port, () => {
   console.log(`Listening: http://localhost:${port}`);
   /* eslint-enable no-console */
 });
-
-module.exports = app;
